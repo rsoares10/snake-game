c4e7513cc3184e590adf20478df7961c8f8beeb4
rsoares10 <rsoares.monteiro10@gmail.com>
2023-02-04 19:50:59 -0300

    Add snake movement behavior

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from turtle import Screen, Turtle
+import time
 
 
 def turtle_builder() -> Turtle:
     t = Turtle()
     t.shape("square")
     t.color("#FFFFFF")
+    t.penup()
     return t
+
+
+segments: list[Turtle] = []
 
 
 def snake_builder() -> None:
@@ -13,6 +18,7 @@ def snake_builder() -> None:
     for pos in starting_positions:
         t = turtle_builder()
         t.goto(pos)
+        segments.append(t)
 
 
 def screen_builder() -> Screen:
@@ -20,13 +26,26 @@ def screen_builder() -> Screen:
     s.setup(width=600, height=600)
     s.bgcolor("#000000")
     s.title("Snake Game")
+    s.tracer(0)
     return s
 
 
 def main() -> None:
     snake = snake_builder()
-
     s = screen_builder()
+
+    game_is_on = True
+    while game_is_on:
+        s.update()
+        time.sleep(0.1)
+
+        for seg_num in range(len(segments) - 1, 0, -1):
+            new_x = segments[seg_num - 1].xcor()
+            new_y = segments[seg_num - 1].ycor()
+            segments[seg_num].goto(new_x, new_y)
+        segments[0].forward(20)
+        segments[0].left(90)
+
     s.exitonclick()
 
 
