0e489a9ea9e5bfc7e7d30415c1c30d6e3ccdd2d2
rsoares10 <rsoares.monteiro10@gmail.com>
2023-02-04 19:08:24 -0300

    Add snake builder

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ def turtle_builder() -> Turtle:
     return t
 
 
+def snake_builder() -> None:
+    starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+    for pos in starting_positions:
+        t = turtle_builder()
+        t.goto(pos)
+
+
 def screen_builder() -> Screen:
     s = Screen()
     s.setup(width=600, height=600)
@@ -17,7 +24,7 @@ def screen_builder() -> Screen:
 
 
 def main() -> None:
-    t = turtle_builder()
+    snake = snake_builder()
 
     s = screen_builder()
     s.exitonclick()
